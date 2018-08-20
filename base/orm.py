6d69eeb9ba3994f25a9c5c4b2c16bc0a9483e3ee
xuejiekun <512559733@qq.com>
2018-08-20 18:07:49 +0800

    change orm

--- base/orm.py
+++ base/orm.py
@@ -1,14 +1,20 @@
 # -*- coding:utf-8 -*-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class BaseORM:
 
-    def __init__(self, database, echo=False, autocommit=True):
+    def __init__(self, database, echo=False, autocommit=False):
         self.engine = create_engine(database, echo=echo)
         DBSession = sessionmaker(bind=self.engine)
         self.session = DBSession(autocommit=autocommit)
+
+    def create_table(self):
+        Base.metadata.create_all(self.engine)
 
     def close(self):
         self.session.close()
